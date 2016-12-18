fde675b73b6396b7f64e9f16586b54c9c2998e18
Adrianzatreanu <zgadi_2007@yahoo.com>
2016-12-18 13:03:19 +0200

    Add tests for requirements

---
+++ tests/GoRequirementTest.py
@@ -0,0 +1,13 @@
+import unittest
+import shutil
+from requirements.GoRequirement import GoRequirement
+
+
+@unittest.skipIf(shutil.which('go') is None, "Go is not installed.")
+class GoRequirementTestCase(unittest.TestCase):
+
+    def test_installed_requirement(self):
+        self.assertTrue(GoRequirement('fmt').is_installed())
+
+    def test_not_installed_requirement(self):
+        self.assertFalse(GoRequirement('some_bad_package').is_installed())
