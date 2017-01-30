40a73da585669e108db79306bacd9d5cc45aa369
vijeth-aradhya <vijthaaa@gmail.com>
2017-01-30 17:47:53 +0530

    PipRequirement.py: Add other pip commands
    Add install, uninstall and upgrade commands.
    
    Fixes https://gitlab.com/coala/package_manager/issues/22

--- dependency_management/requirements/PipRequirement.py
+++ dependency_management/requirements/PipRequirement.py
@@ -49,3 +49,25 @@ class PipRequirement(PackageRequirement):
         return not run(sys.executable + ' -m pip show ' + self.package,
                        stdout=Capture(),
                        stderr=Capture()).returncode
+
+    def install_package(self):
+        """
+        Runs the install command for the package given in a sub-process.
+        """
+        run(" ".join(self.install_command), stdout=Capture(), stderr=Capture())
+
+    def upgrade_package(self):
+        """
+        Runs the upgrade command for the package given in a sub-process.
+        """
+        run(sys.executable + ' -m pip install ' + self.package + ' --upgrade',
+            stdout=Capture(),
+            stderr=Capture())
+
+    def uninstall_package(self):
+        """
+        Runs the uninstall command for the package given in a sub-process.
+        """
+        run(sys.executable + ' -m pip uninstall -y ' + self.package,
+            stdout=Capture(),
+            stderr=Capture())
