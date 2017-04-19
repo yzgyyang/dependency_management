d5373b3036e5df5cebff65f12c9452e91a955fb9
yash-nisar <yash.nisar@somaiya.edu>
2017-04-19 10:00:04 +0530

    Add LuarocksRequirement
    Add LuarocksRequirement as luarocks is a common
    way to manage Lua dependencies.
    
    Closes https://gitlab.com/coala/package_manager/issues/24

---
+++ dependency_management/requirements/LuarocksRequirement.py
@@ -0,0 +1,64 @@
+from dependency_management.requirements.PackageRequirement import (
+    PackageRequirement)
+from sarge import run, Capture
+import platform
+
+
+class LuarocksRequirement(PackageRequirement):
+    """
+    This class is a subclass of ``PackageRequirement``. It specifies the proper
+    type for ``luarocks`` packages automatically and provide a function to
+    check for the requirement.
+    """
+
+    def __init__(self, package, version=''):
+        """
+        Constructs a new ``Luarocks``, using the ``PackageRequirement``
+        constructor.
+
+        >>> pr = LuarocksRequirement('luasocket', '3.0rc1')
+        >>> pr.type
+        'luarocks'
+        >>> pr.package
+        'luasocket'
+        >>> pr.version
+        '3.0rc1'
+        >>> str(pr)
+        'luasocket 3.0rc1'
+
+        :param package: A string with the name of the package to be installed.
+        :param version: A version string. Leave empty to specify latest version.
+        """
+        PackageRequirement.__init__(self, 'luarocks', package, version)
+
+    def install_command(self):
+        """
+        Creates the installation command for the instance of the class.
+
+        >>> LuarocksRequirement('luacheck', '0.19.1').install_command()
+        ['luarocks', 'install', 'luacheck 0.19.1']
+
+        >>> LuarocksRequirement('luacheck').install_command()
+        ['luarocks', 'install', 'luacheck']
+
+        :param return: A string with the installation command.
+        """
+        result = ['luarocks', 'install', self.package + ' ' + self.version
+                  if self.version else self.package]
+        return result
+
+    def is_installed(self):
+        """
+        Checks if the dependency is installed.
+
+        :param return: True if dependency is installed, false otherwise.
+        """
+        cmd = 'luarocks show ' + self.package
+
+        if platform.system() == 'Windows':  # pragma: no cover
+            cmd = 'cmd + /c ' + cmd
+
+        if not run(cmd, stdout=Capture(), stderr=Capture()).returncode:
+            return True
+
+        return False
---
+++ tests/LuarocksRequirementTest.py
@@ -0,0 +1,36 @@
+import unittest
+import unittest.mock
+import sarge
+import shutil
+from dependency_management.requirements.LuarocksRequirement import (
+    LuarocksRequirement)
+
+
+@unittest.skipIf(shutil.which('luarocks') is None,
+                 'luarocks is not installed.')
+class LuarocksRequirementTestCase(unittest.TestCase):
+
+    def test__str__(self):
+        self.assertEqual(str(LuarocksRequirement('luarocks')), 'luarocks')
+        self.assertEqual(str(LuarocksRequirement('luarocks', '2.4.2')),
+                         'luarocks 2.4.2')
+
+    def test_installed_requirement(self):
+        with unittest.mock.patch('dependency_management.requirements.' +
+                                 'LuarocksRequirement.run') as mock:
+            patched = unittest.mock.Mock(spec=sarge.Pipeline)
+            patched.returncode = 0
+            mock.return_value = patched
+            self.assertTrue(LuarocksRequirement('some_good_package').
+                            is_installed())
+
+    def test_not_installed_requirement(self):
+        self.assertFalse(LuarocksRequirement('some_bad_package').is_installed())
+
+
+class LuarocksLuacheckRequirementTestCase(unittest.TestCase):
+
+    def test_install(self):
+        r = LuarocksRequirement('luacheck', '0.19.1')
+        r.install_package()
+        self.assertTrue(r.is_installed())
