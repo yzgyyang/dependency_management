f79f58fdcda4181e9357c2ba757f81380d4a8d68
arjunsinghy96 <14bcs010@smvdu.ac.in>
2017-04-19 22:22:29 +0530

    PipRequirementTest: Add test coverage
    Adds test coverage for new commands 'upgrade_package' and
    'uninstall_package'.
    
    Closes https://gitlab.com/coala/package_manager/issues/69

--- tests/PipRequirementTest.py
+++ tests/PipRequirementTest.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 
 from dependency_management.requirements.PipRequirement import PipRequirement
+from sarge import get_both
 
 
 class PipRequirementTestCase(unittest.TestCase):
@@ -30,3 +31,26 @@ class PipRequirementTestCase(unittest.TestCase):
 
     def test_not_installed_requirement(self):
         self.assertFalse(PipRequirement('some_bad_package').is_installed())
+
+    def test_upgrade_requirement(self):
+        p = PipRequirement('vanity', '1.2.5')
+        p.install_package()
+        old_ver = get_both('pip show vanity')[0].split('\n')[1]
+        p.upgrade_package()
+        new_ver = get_both('pip show vanity')[0].split('\n')[1]
+        self.assertGreater(new_ver, old_ver)
+
+    def test_upgrade_on_already_latest_version(self):
+        p = PipRequirement('vanity')
+        p.install_package()
+        old_ver = get_both('pip show vanity')[0].split('\n')[1]
+        p.upgrade_package()
+        new_ver = get_both('pip show vanity')[0].split('\n')[1]
+        self.assertEqual(new_ver, old_ver)
+
+    def test_uninstall_requirement(self):
+        p = PipRequirement('colorit')
+        p.install_package()
+        self.assertTrue(p.is_installed())
+        p.uninstall_package()
+        self.assertFalse(p.is_installed())
