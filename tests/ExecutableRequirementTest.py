c75052215bb6c9517a17b5eb6a4e90764a58db18
Makman2 <makmanx64@gmail.com>
2017-04-23 23:26:23 +0200

    ExecutableRequirement: Add .bat and .cmd tests
    ... under Windows.
    
    Closes https://gitlab.com/coala/package_manager/issues/94

--- tests/ExecutableRequirementTest.py
+++ tests/ExecutableRequirementTest.py
@@ -1,3 +1,6 @@
+import os
+from platform import system
+from tempfile import TemporaryDirectory
 import unittest
 
 from dependency_management.requirements.ExecutableRequirement import (
@@ -12,3 +15,24 @@ class ExecutableRequirementTestCase(unittest.TestCase):
 
     def test_not_installed_requirement(self):
         self.assertFalse(ExecutableRequirement("some_bad_exec").is_installed())
+
+    @unittest.skipIf(system() != 'Windows', 'Windows-specific test.')
+    def test_installed_batch_requirements(self):
+        for batchfile in ('some_executable.bat', 'some_executable.cmd'):
+            uut = ExecutableRequirement(batchfile)
+
+            self.assertFalse(uut.is_installed())
+
+            with TemporaryDirectory() as tmp:
+                # Create batch file.
+                open(os.path.join(tmp, batchfile), 'w').close()
+
+                # Add batch to path. sys.path doesn't work, changes in this
+                # list aren't picked up.
+                old_path = os.environ['PATH']
+                os.environ['PATH'] = tmp + ';' + old_path
+
+                self.assertTrue(uut.is_installed())
+
+                # Remove again from path.
+                os.environ['PATH'] = old_path
