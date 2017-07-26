285cbd5f4fa44454ddacd3051240ea8d69bd3d7b
Manvendra Singh <manvendra0310@gmail.com>
2017-07-26 16:17:45 +0000

    Helper.py: Fix typo
    Fixes https://gitlab.com/coala/package_manager/issues/158

--- dependency_management/Helper.py
+++ dependency_management/Helper.py
@@ -5,6 +5,6 @@ def is_executable_exists(executable):
     """
     Checks if executable exists in $PATH
 
-    :return: True if executable eixsts in $PATH, false otherwise
+    :return: True if executable exists in $PATH, false otherwise
     """
     return shutil.which(executable) is not None
