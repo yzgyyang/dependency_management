471da6a37d79681c4261e2ead3ea9c236e31a074
Nalin Bhardwaj <maverick.nalin@gmail.com>
2017-12-01 23:09:49 +0530

    HaskellRequirement.py: Create HaskellRequirement
    Support cabal and stack for Haskell requirements by deprecating
    CabalRequirement for HaskellRequirement
    
    Closes https://gitlab.com/coala/package_manager/issues/67

---
+++ dependency_management/requirements/HaskellRequirement.py
@@ -0,0 +1,215 @@
+from distutils.version import LooseVersion
+import re
+
+from sarge import run, Capture
+
+from dependency_management.Helper import is_executable_exists
+from dependency_management.requirements.AnyOneOfRequirements import (
+    AnyOneOfRequirements)
+from dependency_management.requirements.ExecutableRequirement import (
+    ExecutableRequirement)
+from dependency_management.requirements.PackageRequirement import (
+    PackageRequirement)
+
+
+class NoArgsNotImplementedError(NotImplementedError, TypeError):
+    pass
+
+
+class HaskellRequirement(PackageRequirement):
+    """
+    This class is a subclass of ``PackageRequirement``. It specifies the
+    proper type automatically.
+    """
+
+    """
+    List of supported package manager in order of preference for use.
+    """
+    SUPPORTED_PACKAGE_MANAGERS = ['stack', 'cabal']
+    _available_managers = None
+
+    REQUIREMENTS = {
+        AnyOneOfRequirements(
+            list(
+                ExecutableRequirement(name)
+                for name in SUPPORTED_PACKAGE_MANAGERS
+            )
+        ),
+    }
+
+    """
+    List of commands that can be used to verify if the package is installed.
+    """
+    CHECKER_COMMANDS = {
+        'cabal': 'cabal info "{}"',
+        'stack': 'stack exec ghc-pkg -- list',
+    }
+    """
+    List of commands that need to be searched later for package.
+    """
+    CHECKER_SEARCH = {
+        'cabal': False,
+        'stack': True,
+    }
+
+    """
+    List of commands that can be used to install a package.
+    """
+    _INSTALL_COMMANDS = {
+        'cabal': ('cabal', 'install'),
+        'stack': ('stack', 'install'),
+    }
+
+    """
+    List of regex patterns to used with CHECKER_COMMAND to extract the version
+    """
+    VERSION_EXTRACTION_REGEX = {
+        'cabal': r'Versions\sinstalled:\s(?P<version>[^\s]+)(?:\s|,|$)',
+        'stack': r'-(?P<version>[^\s]+)(?:\s|$)'
+    }
+
+    def __init__(self, package: str=None, version=''):
+        """
+        Constructs a new ``HaskellRequirement``, using the
+        ``PackageRequirement`` constructor.
+
+        >>> hr = HaskellRequirement('random')
+        >>> hr.package
+        'random'
+
+        :param package: A string with the name of the package to be installed.
+        :param version: A version string.
+        """
+        self._manager = None
+
+        PackageRequirement.__init__(self, 'haskell', package, version)
+
+    def get_available_package_manager(self):
+        """
+        Returns the available package manager that can be used to satisfy
+        the requirement.
+
+        Raises NotImplementedError if there's no supported package manager.
+
+        :return: Supported package manager
+        """
+        if not self._manager:
+            self._manager = self.available_package_managers[0]
+        return self._manager
+
+    @property
+    def available_package_managers(self):
+        """
+        Return all available package managers.
+
+        :raises NotImplementedError:
+             There are no package managers for requirement.
+        """
+        if self._available_managers is None:
+            self._available_managers = list(
+                self.get_all_available_package_managers())
+        return self._available_managers
+
+    @classmethod
+    def get_all_available_package_managers(self):
+        """
+        Yield the available package managers.
+
+        :raises NotImplementedError:
+            There are no supported package managers.
+        """
+        found = False
+        for manager in self.SUPPORTED_PACKAGE_MANAGERS:
+            if is_executable_exists(manager):
+                found = True
+                yield manager
+        if found:
+            return
+
+        raise NotImplementedError(
+            "This platform doesn't have any of the supported package "
+            'manager(s): {}'
+            .format(', '.join(self.SUPPORTED_PACKAGE_MANAGERS)))
+
+    def get_installed_version(self):
+        """
+        Return the version if the package is installed, an empty string
+        otherwise.
+        """
+        package_manager = self.get_available_package_manager()
+        package = self.package
+
+        results = run(self.CHECKER_COMMANDS[package_manager].format(package),
+                      stdout=Capture(), stderr=Capture())
+
+        output = results.stdout.text
+        regex = self.VERSION_EXTRACTION_REGEX[package_manager]
+
+        if self.CHECKER_SEARCH[package_manager]:
+            output = re.search(re.escape(package) + regex, output)
+            if output is None:
+                return ''
+
+        elif results.returncode:
+            return ''
+
+        res = re.search(regex, str(output))
+        if res:
+            output = res.group('version')
+
+        return output.rstrip()
+
+    def is_installed(self):
+        """
+        Check if the requirement is satisfied by calling various package
+        managers that are mentioned.
+
+        Raises NotImplementedError if executable is not defined when
+        there's no supported package manager.
+
+        :return: True if the dependency is installed, false otherwise
+        """
+
+        package_manager = self.get_available_package_manager()
+        package = self.package
+        version = self.version
+
+        if version:
+            installed_version = self.get_installed_version()
+            if installed_version:
+                return LooseVersion(installed_version) >= LooseVersion(version)
+            else:
+                return False
+
+        results = run(self.CHECKER_COMMANDS[package_manager].format(package),
+                      stdout=Capture(), stderr=Capture())
+        regex = self.VERSION_EXTRACTION_REGEX[package_manager]
+
+        if self.CHECKER_SEARCH[package_manager]:
+            output = re.search(re.escape(package) + regex, results.stdout.text)
+            return output is not None
+
+        else:
+            return not results.returncode
+
+    def install_command(self):
+        """
+        Creates the installation command for the instance of the class.
+
+        >>> HaskellRequirement('foo', '').install_command()
+        ['stack', 'install', 'foo']
+        >>> HaskellRequirement('foo', '3').install_command()
+        ['stack', 'install', 'foo-3']
+
+        :param return: A list with the installation command.
+        """
+        package_manager = self.get_available_package_manager()
+        suffix = self.package
+        if self.version:
+            suffix += '-' + self.version
+
+        command = list(self._INSTALL_COMMANDS[package_manager])
+
+        command.append(suffix)
+
+        return command
---
+++ tests/HaskellRequirementTest.py
@@ -0,0 +1,135 @@
+import shutil
+import unittest
+
+import sarge
+
+from dependency_management.Helper import is_executable_exists
+from dependency_management.requirements.HaskellRequirement import (
+    HaskellRequirement)
+
+
+@unittest.skipIf(shutil.which('cabal') is None, 'Cabal is not installed.')
+class HaskellCabalRequirementTestCase(unittest.TestCase):
+
+    def _mock_test(self, package: str=None, version=''):
+        _shutil_which = shutil.which
+        hr = HaskellRequirement(package, version)
+        old_managers = hr._available_managers
+        hr._manager = None
+
+        hr._available_managers = None
+
+        def fake_which(exe):
+            if exe == 'cabal':
+                return True
+
+        try:
+            shutil.which = fake_which
+
+            # Force caching of package manager
+            hr.get_available_package_manager()
+            return hr
+        finally:
+            # Reverse changes
+            hr._available_managers = old_managers
+            shutil.which = _shutil_which
+
+    def test__str__(self):
+        r = self._mock_test('random')
+        self.assertEqual(str(r), 'random')
+        self.assertEqual(r._manager, 'cabal')
+
+    def test_installed_requirement(self):
+        r = self._mock_test('array')
+        self.assertTrue(r.is_installed())
+        self.assertEqual(r._manager, 'cabal')
+
+    def test_not_installed_requirement(self):
+        r = self._mock_test('some_bad_package')
+        self.assertFalse(r.is_installed())
+        self.assertEqual(r._manager, 'cabal')
+
+    def test_installed_requirement_version(self):
+        r = self._mock_test('array', '0')
+        self.assertTrue(r.is_installed())
+        self.assertEqual(r._manager, 'cabal')
+
+    def test_not_installed_requirement_because_version(self):
+        r = self._mock_test('array', '1000000')
+        self.assertFalse(r.is_installed())
+        self.assertEqual(r._manager, 'cabal')
+
+    def test_not_installed_requirement_with_version(self):
+        r = self._mock_test('some_bad_package', '1')
+        self.assertFalse(r.is_installed())
+        self.assertEqual(r._manager, 'cabal')
+
+
+@unittest.skipIf(shutil.which('stack') is None, 'Stack is not installed.')
+class HaskellStackRequirementTestCase(unittest.TestCase):
+
+    def test__str__(self):
+        self.assertEqual(str(HaskellRequirement(
+            'random', '')), 'random')
+
+    def test_installed_requirement(self):
+        self.assertTrue(HaskellRequirement('array', '').is_installed())
+
+    def test_not_installed_requirement(self):
+        self.assertFalse(
+            HaskellRequirement('some_bad_package', '').is_installed())
+
+    def test_installed_requirement_version(self):
+        self.assertTrue(HaskellRequirement(
+            'array', '0').is_installed())
+
+    def test_not_installed_requirement_because_version(self):
+        self.assertFalse(HaskellRequirement('array',
+                                            '1000000').is_installed())
+
+    def test_not_installed_requirement_with_version(self):
+        self.assertFalse(HaskellRequirement('some_bad_package', '1')
+                         .is_installed())
+
+
+class ExpectedErrorsHaskellRequirementTestCase(unittest.TestCase):
+
+    NO_SUPPORTED_PACKAGE_MANAGER_RE = ("This platform doesn't have any of the "
+                                       'supported package manager')
+
+    def _mock_test(self, managers, exc=None, message=None):
+        _shutil_which = shutil.which
+        hr = HaskellRequirement('something_random')
+        old_managers = hr._available_managers
+
+        hr._manager = None
+        hr._available_managers = None
+
+        def fake_which(exe):
+            if exe in managers:
+                return True
+        try:
+            shutil.which = fake_which
+
+            if not exc:
+                # Force caching of all package managers
+                hr.available_package_managers
+                return hr
+
+            with self.assertRaisesRegex(
+                    exc,
+                    message):
+                hr.get_available_package_manager()
+        finally:
+            # Reverse changes
+            hr._available_managers = old_managers
+            shutil.which = _shutil_which
+
+    def test_platform_without_supported_package_manager(self):
+        self._mock_test([], NotImplementedError,
+                        self.NO_SUPPORTED_PACKAGE_MANAGER_RE)
+
+    def test_platform_with_multiple_package_manager(self):
+        managers = HaskellRequirement.SUPPORTED_PACKAGE_MANAGERS
+        r = self._mock_test(managers)
+        self.assertCountEqual(r.available_package_managers, managers)
