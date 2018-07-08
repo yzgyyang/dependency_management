77c50517b0a47f772950bf13bff5d665389ecb52
Guangyuan Yang <yzgyyang@outlook.com>
2018-07-08 14:28:54 -0400

    .ci/common.sh: move CI common setup into a script
    Closes https://gitlab.com/coala/package_manager/issues/191

---
+++ .ci/deps.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -xe
+
+# Need $PYTHON
+
+$PYTHON -m pip install --upgrade pip setuptools
+npm install -g bower
+curl -fsSL -o haskellstack.sh https://get.haskellstack.org/
+curl -fsSL -o miniconda.sh \
+     http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x haskellstack.sh miniconda.sh
+./haskellstack.sh
+stack setup --resolver ghc-8.4.2
+./miniconda.sh -b -p "$HOME/miniconda"
+go get -u github.com/gpmgo/gopm
+go install github.com/gpmgo/gopm
+cpan App::cpanminus
+echo 'pytest-spec' >> test-requirements.txt
+$PYTHON -m pip install -r requirements.txt -r test-requirements.txt
